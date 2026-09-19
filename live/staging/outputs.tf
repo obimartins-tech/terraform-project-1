a84e59d0033c01f1a2ab4fac3fe496c3c9251155
obimartins-tech <obimartins.tech@gmail.com>
2026-09-19 13:17:27 +0000

    Duplicate provider configuration corrected

--- live/staging/outputs.tf
+++ live/staging/outputs.tf
@@ -1,3 +1,24 @@
-provider "aws" {
-  region = var.aws_region
+output "vpc_id" {
+  description = "ID of the development VPC"
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_id" {
+  description = "ID of the development public subnet"
+  value       = module.network.public_subnet_id
+}
+
+output "instance_id" {
+  description = "ID of the development EC2 instance"
+  value       = module.web_server.instance_id
+}
+
+output "public_ip" {
+  description = "Public IP of the development web server"
+  value       = module.web_server.public_ip
+}
+
+output "website_url" {
+  description = "URL of the development web server"
+  value       = "http://${module.web_server.public_ip}"
 }
