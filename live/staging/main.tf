1d20ff6567ec5e03a9c9216936c004cba2832000
obimartins-tech <obimartins.tech@gmail.com>
2026-09-22 07:09:56 +0000

    Fixed main.tf file

--- live/staging/main.tf
+++ live/staging/main.tf
@@ -21,10 +21,10 @@ data "aws_ami" "ubuntu" {
 module "network" {
   source = "../../modules/network"
 
-  project_name        = var.project_name
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidr  = var.public_subnet_cidr
-  availability_zone   = var.availability_zone
+  project_name       = var.project_name
+  vpc_cidr           = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidr
+  availability_zone  = var.availability_zone
 }
 
 module "security_group" {
