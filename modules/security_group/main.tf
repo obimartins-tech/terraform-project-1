8260be63aa6f526f7b6eb5c5ff1792958cb95762
obimartins-tech <obimartins.tech@gmail.com>
2026-09-23 14:19:56 +0000

    Checkov-test-2 corrected

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,12 +1,8 @@
+#checkov:skip=CKV_AWS_260:Public HTTP access is intentionally required because this EC2 instance hosts a publicly accessible web server.
 resource "aws_security_group" "web" {
   name        = "${var.project_name}-web-sg"
   description = "Allow HTTP traffic to web server"
   vpc_id      = var.vpc_id
-
-#checkov:skip=CKV_AWS_260:Public HTTP access is intentionally required because this EC2 instance hosts a publicly accessible web server.
-resource "aws_security_group" "web" {
-  name   = "web-sg"
-  vpc_id = var.vpc_id
 
 
   ingress {
