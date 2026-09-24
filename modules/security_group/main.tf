a2c5f4a3050c922f5cb617c931402b8f0a2eb7af
obimartins-tech <obimartins.tech@gmail.com>
2026-09-24 12:56:21 +0000

    Checkov-test-12, Surpressed  CKV2_AWS_5: Ensure that Security Groups are attached to another resource

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,7 +1,8 @@
 resource "aws_security_group" "web" {
   #checkov:skip=CKV_AWS_260:Public HTTP access is intentionally required because this EC2 instance hosts a publicly accessible web server.
   #checkov:skip=CKV_AWS_382:Unrestricted outbound access is intentionally required for this web server to retrieve updates, packages, dependencies, and external resources.
-  
+  #checkov:skip=CKV_AWS_5:Security Groups are attached to other resources.
+
   name        = "${var.project_name}-web-sg"
   description = "Allow HTTP traffic to web server"
   vpc_id      = var.vpc_id
