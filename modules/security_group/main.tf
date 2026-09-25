801543a64198f1b2d8e11af6767180d2f8c9b83a
Obi Martins <73021405+ObiMartins@users.noreply.github.com>
2026-09-25 08:10:17 +0000

    Checkov-test-12, corrected

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,7 +1,10 @@
 resource "aws_security_group" "web" {
+  #checkov:skip=CKV2_AWS_5:Security group is intentionally attached to the EC2 web server through the web_server module.
+
   #checkov:skip=CKV_AWS_260:Public HTTP access is intentionally required because this EC2 instance hosts a publicly accessible web server.
+  
   #checkov:skip=CKV_AWS_382:Unrestricted outbound access is intentionally required for this web server to retrieve updates, packages, dependencies, and external resources.
-  #checkov:skip=CKV_AWS_5:Security Groups are attached to other resources.
+ 
 
   name        = "${var.project_name}-web-sg"
   description = "Allow HTTP traffic to web server"
